f232d89e71a42b74c33582c0f708fe4ee7922a80
Alexander Bispo <alexbispo173@gmail.com>
2019-12-09 16:05:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
 
      if req.path.match(/items/)
  
-      song_title = req.path.split("/items/").last 
-      song = @@items.find{|s| s.title == song_title}
+      item_name = req.path.split("/items/").last 
+      name = @@items.find{|s| s.name == item_name}
  
       resp.write song.artist
     end

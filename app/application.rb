f7b1f7f0bda1dc31745dd17e74f3f8aca4b7d959
Alexander Bispo <alexbispo173@gmail.com>
2019-12-09 16:03:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,13 @@ class Application
      req = Rack::Request.new(env)
      resp = Rack::Response.new
 
-     
+     if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
 
      resp.finish
    end
